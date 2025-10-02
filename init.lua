28627c9ad956d0ee03a420f07a2e7e7c74a6261d
Trent Davies <trent.davies@gmail.com>
2025-10-02 14:33:12 -0600

    updated options

--- init.lua
+++ init.lua
@@ -1,11 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
-require 'options'
-
--- [[ Basic Keymaps ]]
+require 'core.options'
 require 'keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
@@ -14,5 +10,5 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- The line beneath this fs called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
